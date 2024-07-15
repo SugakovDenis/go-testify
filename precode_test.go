3e471ef227a5ae81ce82a87c119a6192bae922c2
Sugakov Denis <Sugakovdv@yandex.ru>
2024-07-15 15:24:45 +0300

    исправил тесты

--- precode_test.go
+++ precode_test.go
@@ -11,15 +11,13 @@ import (
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, responseRecorder.Code, http.StatusOK)
 	body := responseRecorder.Body.String()
-	require.NotEmpty(t, body)
 
 	cafes := strings.Split(body, ",")
 	require.Len(t, cafes, totalCount)
@@ -27,12 +25,12 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 }
 
 func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscaw", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
-
 	handler.ServeHTTP(responseRecorder, req)
+
 	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 
 	expectedMessage := "wrong city value"
@@ -42,15 +40,14 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 
 func TestMainHandlerWhenBadStatusCode(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
-	responseRecorder := httptest.NewRecorder()
 
+	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
-	expectedMessage := "status code 400"
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 
 	body := responseRecorder.Body.String()
-	assert.Equal(t, body, expectedMessage)
+	require.NotEmpty(t, body)
 
 }
